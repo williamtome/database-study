8f835a99dbac8c8d3d0554f0e89d14c4220d6472
William Tomé <william.tomenh@gmail.com>
2023-04-19 08:01:38 -0300

    feat: add more update queries.

--- UPDATE.sql
+++ UPDATE.sql
@@ -1,10 +1,21 @@
-# UPDATE atualiza os registros. Porém é muito importante essa instrução vir acompanhado do WHERE para evitar atualizar dados indesejados.
+# UPDATE atualiza os registros. Porém é muito importante essa
+# instrução vir acompanhado do WHERE para evitar atualizar dados indesejados.
 UPDATE produtos SET preco = 9956.54 WHERE id = 4;
 UPDATE produtos SET estoque = 1 WHERE id = 1;
+UPDATE produtos SET fornecedor_id = 10 WHERE id = 13;
+UPDATE produtos SET fornecedor_id = 8 WHERE id = 14;
 
-# Deve ter muito cuidado com o UPDATE pois senão atualizará um registro que não estava previsto!
+# Deve ter muito cuidado com o UPDATE pois senão atualizará um
+# registro que não estava previsto!
 UPDATE usuarios SET nome = 'Keyla' WHERE data_cadastro LIKE '2022-01-%';
 UPDATE usuarios SET nome = 'João' WHERE id = 2;
 UPDATE usuarios SET nome = 'Jéssica' WHERE id = 3;
+UPDATE usuarios SET nome = 'João Bosco' WHERE id = 2;
+UPDATE fornecedor SET telefone = '(43) 1234-5678' WHERE id = 10;
 UPDATE fornecedor SET nome = 'Positivo' WHERE id = 4;
 UPDATE fornecedor SET telefone = '(51) 99099-3422' WHERE id = 7;
+
+# É possível atualizar mais de um campo da tabela:
+UPDATE usuarios SET nome = 'Jéssica da Silva', data_cadastro = NOW() WHERE id = 3;
+
+UPDATE usuarios SET nome = 'Keyla Menezes' WHERE nome LIKE 'Keyla%';
